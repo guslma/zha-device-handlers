294c7b75327529c9a7c02228b9f713c76683f66a
Gustavo Lima <141639311+guslma@users.noreply.github.com>
2025-03-24 13:02:13 -0300

    Update ts0601_cover.py

--- zhaquirks/tuya/ts0601_cover.py
+++ zhaquirks/tuya/ts0601_cover.py
@@ -620,3 +620,41 @@ class TuyaCloneCover0601(TuyaWindowCover):
             }
         }
     }
+
+class NovaDigitalSmartCoverTS0105(TuyaWindowCover):
+    """Nova Digital motor Tuya TS0105 (_TZE600_ogyg1y6b_)."""
+
+    signature = {
+        MODELS_INFO: [
+            ("_TZE600_ogyg1y6b", "TS0105"),
+        ],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: 0x0051,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Time.cluster_id,
+                    TuyaManufCluster.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            },
+        },
+    }
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                DEVICE_TYPE: zha.DeviceType.WINDOW_COVERING_DEVICE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    Time.cluster_id,
+                    TuyaManufacturerWindowCover,
+                    TuyaWindowCoverControl,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            },
+        },
+    }
