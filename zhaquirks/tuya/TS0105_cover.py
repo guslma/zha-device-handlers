42efb98f83e697752a63a343e56e350907b005d7
Gustavo Lima <141639311+guslma@users.noreply.github.com>
2025-03-24 19:13:35 -0300

    Create TS0105_cover.py

---
+++ zhaquirks/tuya/TS0105_cover.py
@@ -0,0 +1,56 @@
+"""Tuya based cover and blinds."""
+
+from zigpy.profiles import zha
+from zigpy.zcl.clusters.general import Basic, Groups, Identify, OnOff, Ota, Scenes, Time
+
+from zhaquirks.const import (
+    DEVICE_TYPE,
+    ENDPOINTS,
+    INPUT_CLUSTERS,
+    MODELS_INFO,
+    OUTPUT_CLUSTERS,
+    PROFILE_ID,
+)
+from zhaquirks.tuya import (
+    TuyaManufacturerWindowCover,
+    TuyaManufCluster,
+    TuyaWindowCover,
+    TuyaWindowCoverControl,
+)
+
+class NovaDigitalSmartCoverTS0105(TuyaWindowCover):
+    """Nova Digital motor Tuya TS0105 (_TZE600_ogyg1y6b_)."""
+
+    signature = {
+        MODELS_INFO: [
+            ("_TZE600_ogyg1y6b", "TS0105"),
+        ],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Time.cluster_id,
+                    TuyaManufCluster.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            },
+        },
+    }
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                DEVICE_TYPE: zha.DeviceType.WINDOW_COVERING_DEVICE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Time.cluster_id,
+                    TuyaManufacturerWindowCover,
+                    TuyaWindowCoverControl,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            },
+        },
+    }
