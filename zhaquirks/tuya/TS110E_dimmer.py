77894da3f1b7b9cfb676be295411320ccdd440be
Gustavo Lima <141639311+guslma@users.noreply.github.com>
2025-08-20 15:15:59 -0300

    Update TS110E_dimmer.py

--- zhaquirks/tuya/TS110E_dimmer.py
+++ zhaquirks/tuya/TS110E_dimmer.py
@@ -13,7 +13,8 @@ from zigpy.zcl.clusters.general import (
     Scenes,
     Time,
 )
-
+from zhaquirks.tuya import TuyaManufCluster
+from zhaquirks import NoReplyMixin
 from zhaquirks.const import (
     DEVICE_TYPE,
     ENDPOINTS,
@@ -22,16 +23,16 @@ from zhaquirks.const import (
     OUTPUT_CLUSTERS,
     PROFILE_ID,
 )
-from zhaquirks.tuya import TuyaManufCluster
 
 
 class SafeOnOffCluster(OnOff, CustomCluster):
+    """Safe OnOff cluster for Tuya TS110E Dimmer that blocks automatic OFF."""
+
     def _update_attribute(self, attrid, value):
         if attrid == 0x0000 and value == 0:
             self.debug("Ignorando OFF automático")
-            return  # Bloqueia o OFF
+            return
         super()._update_attribute(attrid, value)
-
 
 class DimmerSwitch(CustomDevice):
     """Quirk for Tuya TS110E Dimmer."""
@@ -62,7 +63,8 @@ class DimmerSwitch(CustomDevice):
             #  <SimpleDescriptor endpoint=242 profile=41376 device_type=97
             #  input_clusters=[]
             #  output_clusters=[33]>
-            242: {
+           242: {
+                
                 PROFILE_ID: zgp.PROFILE_ID,
                 DEVICE_TYPE: zgp.DeviceType.PROXY_BASIC,
                 INPUT_CLUSTERS: [],
