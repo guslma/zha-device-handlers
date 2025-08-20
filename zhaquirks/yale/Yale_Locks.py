87400390850e07e92b4f9013f2b4cbb1ac49e714
Gustavo Lima <141639311+guslma@users.noreply.github.com>
2025-08-20 15:11:29 -0300

    Update Yale_Locks.py

--- zhaquirks/yale/Yale_Locks.py
+++ zhaquirks/yale/Yale_Locks.py
@@ -21,14 +21,12 @@ from zhaquirks.const import (
     PROFILE_ID,
 )
 
-
 class YaleCluster(CustomCluster):
     """Proprietary Yale cluster (0x100F)."""
 
     cluster_id = 0x100F
     name = "Yale Proprietary Cluster"
     ep_attribute = "yale_cluster"
-
 
 class YaleLock(CustomDevice):
     """Yale YDM60 / YMC 420 D Locks."""
@@ -86,6 +84,7 @@ class YaleLock(CustomDevice):
                     PowerConfiguration.cluster_id,
                     YaleCluster,
                 ],
+
             }
         }
     }
