208fb732c1f2d419c3c90cb9a0297ecba9ca09d7
Gustavo Lima <141639311+guslma@users.noreply.github.com>
2025-08-21 11:30:45 -0300

    Update TS110E_dimmer.py

--- zhaquirks/tuya/TS110E_dimmer.py
+++ zhaquirks/tuya/TS110E_dimmer.py
@@ -30,8 +30,8 @@ class SafeOnOffCluster(OnOff, CustomCluster):
 
     def _update_attribute(self, attrid, value):
         if attrid == 0x0000 and value == 0:
-            self.debug("Ignorando OFF automático")
-            return  # Bloqueia o OFF
+            self.debug("Ignoring automatic OFF")
+            return
         super()._update_attribute(attrid, value)
 
 
